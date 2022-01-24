1f309c9db0707372d509ef998f3d17c86c599b74
Quinn Dawkins <quinn.dawkins@gmail.com>
2022-01-24 14:30:36 -0500

    missing import

--- examples/regular_tree_iso_timing.py
+++ examples/regular_tree_iso_timing.py
@@ -1,6 +1,7 @@
 from diffusion_source.infection_model import FixedTSI
 from diffusion_source.graphs import RegularTree
 from diffusion_source.discrepancies import L2_h
+import sys
 
 k_iso = int(sys.argv[1])
 m = int(sys.argv[2])
@@ -12,5 +13,5 @@ I = FixedTSI(G, L2_h, T=150, m=m, d1=False, iso=True, k_iso=k_iso)
 source = I.select_uniform_source()
 x = I.data_gen(source)
 
-I.p_values(x, meta=("k: {}".format(k_iso), x, s))
+I.p_values(x, meta=("k: {}".format(k_iso), x, source))
 I.store_results("results/iso_timing_results/{}_{}_{}.p".format("regular_tree", m, k_iso))
